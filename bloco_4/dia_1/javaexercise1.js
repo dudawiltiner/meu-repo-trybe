b465b7b03d8d8ad0da2128053806334dd5c34873
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-02-24 18:57:59 -0300

    Armazena os Exercícios Java Script

---
+++ bloco_4/dia_1/javaexercise1.js
@@ -0,0 +1,10 @@
+const Name = "Eduarda";
+const birthCity = "Salvador";
+let birthYear = 1998;
+
+birthYear= 2020;
+birthCity = "Feira";
+
+console.log(Name);
+console.log(birthCity);
+console.log(birthYear);
