31ec02e430464c7c9350caa1903c670d0e0272eb
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-05 16:02:15 -0300

    Adciona Exercício 1

---
+++ bloco_5/dia_3/script.js
@@ -0,0 +1,35 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+
+for(let index = 0; index < dezDaysList.length; index++){
+    let listDays = document.createElement('li');
+    listDays.innerHTML = dezDaysList[index];
+    listDays.className = 'day';
+    if(dezDaysList[index] == 24 || dezDaysList[index] == 31){
+      listDays.className = 'day holiday';
+    }
+    else if(dezDaysList[index] == 4 || dezDaysList[index] == 11 ||dezDaysList[index] == 18){
+      listDays.className = 'day friday';
+    }
+    else if(dezDaysList[index] == 25){
+      listDays.className = 'day holiday friday';
+    }
+    document.querySelector('#days').appendChild(listDays);
+}
